81bf87021920f197e5000dd375db5a26ebabc270
Satyendra Kumar Namdeo <satyendranamdeo78130@gmail.com>
2026-09-21 18:32:04 +0530

    chore: add Windows local agent launcher

---
+++ start_sanvi_agent.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "%~dp0"
+if not exist ".venv\Scripts\python.exe" (py -3.11 -m venv .venv & .venv\Scripts\python.exe -m pip install -r requirements.txt)
+.venv\Scripts\python.exe local_agent.py
+pause
